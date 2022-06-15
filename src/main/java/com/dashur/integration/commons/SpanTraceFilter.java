390bae537c860c40b3d5ca441e581ee120d45c68
Henrik Meijer <henrik@elysiumstudios.se>
2022-06-15 08:40:24 +0200

    ADD log trace to SpanTraceFilter and vert.x newer version

--- src/main/java/com/dashur/integration/commons/SpanTraceFilter.java
+++ src/main/java/com/dashur/integration/commons/SpanTraceFilter.java
@@ -9,14 +9,17 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 import org.jboss.logmanager.MDC;
+import lombok.extern.slf4j.Slf4j;
 
 @Provider
+@Slf4j
 public class SpanTraceFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
   @Context HttpServerRequest request;
 
   @Override
   public void filter(ContainerRequestContext requestContext) throws IOException {
+    log.debug("SpanTraceFilter.filter(ContainerRequestContext)");
     String traceId = request.getHeader("x-b3-traceid");
     if (traceId != null) {
       MDC.put("x-b3-traceid", traceId);
@@ -31,6 +34,7 @@ public class SpanTraceFilter implements ContainerRequestFilter, ContainerRespons
   public void filter(
       ContainerRequestContext requestContext, ContainerResponseContext responseContext)
       throws IOException {
+    log.debug("SpanTraceFilter.filter(ContainerRequestContext, ContainerResponseContext)");
     MDC.remove("x-b3-traceid");
     MDC.remove("x-b3-spanid");
   }
